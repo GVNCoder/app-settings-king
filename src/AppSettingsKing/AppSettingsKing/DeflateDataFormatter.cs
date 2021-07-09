87cfea00cb51df5df95ce5de908e773c1f0b0396
DmitrievStanislav <dmitrievsvp@gmail.com>
2021-07-09 19:24:08 +0300

    Added Deflate data formatter

---
+++ src/AppSettingsKing/AppSettingsKing/DeflateDataFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace AppSettingsKing
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DeflateDataFormatter : IDataFormatter
+    {
+        public byte[] Process(byte[] buffer)
+        {
+            using (var inputStream = new MemoryStream(buffer))
+            using (var compressStream = new MemoryStream())
+            using (var compressor = new DeflateStream(compressStream, CompressionMode.Compress))
+            {
+                inputStream.CopyTo(compressor);
+                compressor.Flush();
+
+                return compressStream.ToArray();
+            }
+        }
+
+        public byte[] ProcessBack(byte[] buffer)
+        {
+            using (var inputStream = new MemoryStream(buffer))
+            using (var decompressedStream = new MemoryStream())
+            using (var decompressionStream = new DeflateStream(inputStream, CompressionMode.Decompress))
+            {
+                decompressionStream.CopyTo(decompressedStream);
+                return decompressedStream.ToArray();
+            }
+        }
+    }
+}
--- src/AppSettingsKing/libTest/Program.cs
+++ src/AppSettingsKing/libTest/Program.cs
@@ -7,17 +7,17 @@ namespace libTest
     {
         public static void Main(string[] args)
         {
-            var settingsFile = new AppSettingsFile("testFile.bruh");
+            var settingsFile = new AppSettingsFile("testFile.bruh", new DeflateDataFormatter());
 
-            //using (var dataStream = new MemoryStream())
-            //using (var writer = new BinaryWriter(dataStream))
-            //{
-            //    writer.Write(255);
+            using (var dataStream = new MemoryStream())
+            using (var writer = new BinaryWriter(dataStream))
+            {
+                writer.Write("Some text here bro!");
 
-            //    settingsFile.CreateEntry("bum.txt", dataStream.ToArray());
-            //}
+                settingsFile.CreateEntry("bum.txt", dataStream.ToArray());
+            }
 
-            //settingsFile.Save();
+            settingsFile.Save();
             settingsFile.Load();
         }
     }
